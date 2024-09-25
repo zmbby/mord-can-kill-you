c8a8c156fad88016b3cdbb6779831fb953bce24f
zombieBOY <zoubairgaming5@gmail.com>
2024-09-25 07:05:05 +0100

    ss

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zm <zm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/30 03:47:51 by zm                #+#    #+#             */
-/*   Updated: 2024/09/25 06:16:43 by zm               ###   ########.fr       */
+/*   Updated: 2024/09/25 06:53:48 by zm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,6 @@
 
 int	main(void)
 {
-	char	src[20];
-
-	void	*dest;
-	dest = ft_memalloc(20);
-	int i = 0;
-	while (i < 20)
-	{
-		((char *)dest)[i] = i + 48;
-		i++;
-	}
-	ft_putstr(dest);
-	ft_memdel(dest);
-	
+	ft_putnbr(ft_atoi("123"));
 	return (0);
 }
