740ecb8139de9a3492ef23af5d0832309da2823e
zombieBOY <zoubairgaming5@gmail.com>
2024-09-30 14:10:07 +0100

    ss

--- ft_isupper.c
+++ ft_isupper.c
@@ -6,11 +6,11 @@
 /*   By: zm <zm@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 01:30:32 by zm                #+#    #+#             */
-/*   Updated: 2024/09/30 14:06:41 by zoentifi         ###   ########.fr       */
+/*   Updated: 2024/09/30 14:09:53 by zoentifi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h" // machi darori dir include hna
+#include "libft.h" // machi darori dir include l libft f ft_isupper
 
 int	ft_isupper(int c)
 {
